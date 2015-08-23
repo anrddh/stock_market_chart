c280b277b4627a4c8609d94db0408cc873511c7c
Aniruddh Agarwal <aaniruddh99@gmail.com>
2015-08-23 12:07:07 +0800

    fixed bug where deleted stocks couldn't be added again

--- public/js/controllers/mainController.js
+++ public/js/controllers/mainController.js
@@ -3,6 +3,9 @@ Stock.controller("mainController", function($scope, $http) {
     $scope.data = [];
     $scope.series = [];
 
+    $scope.DelData = [];
+    $scope.DelSeries = [];
+
     for(i = 1; i < 61; i++) {
         $scope.labels.push(i);
     }
@@ -20,23 +23,38 @@ Stock.controller("mainController", function($scope, $http) {
         });
 
     $scope.addStock = function() {
-        $http.get('/api/stock/' + $scope.stock)
-            .success(function(data) {
-                var dataSet = data.data.slice(0, 60);
-                dataSet = dataSet.map(function(item) {
-                    return item[4];
+        if($scope.DelSeries.indexOf($scope.stock) !== -1) {
+            var index = $scope.DelSeries.indexOf($scope.stock);
+
+            $scope.data.push($scope.DelData[index]);
+            $scope.series.push($scope.DelSeries[index]);
+
+            $scope.DelData.splice(index, 1);
+            $scope.DelSeries.splice(index, 1);
+
+            $scope.stock = "";
+        } else {
+            $http.get('/api/stock/' + $scope.stock)
+                .success(function(data) {
+                    var dataSet = data.data.slice(0, 60);
+                    dataSet = dataSet.map(function(item) {
+                        return item[4];
+                    });
+                    $scope.data.push(dataSet);
+                    $scope.series.push($scope.stock);
+                    $scope.stock = '';
+                })
+                .error(function(data) {
+                    console.log('Error: ' + data);
                 });
-                $scope.data.push(dataSet);
-                $scope.series.push($scope.stock);
-                $scope.stock = '';
-            })
-            .error(function(data) {
-                console.log('Error: ' + data);
-            });
+        }
     }
 
     $scope.removeStock = function(stock) {
         var index = $scope.series.indexOf(stock);
+
+        $scope.DelData.push($scope.data[index]);
+        $scope.DelSeries.push($scope.series[index]);
 
         $scope.data.splice(index, 1);
         $scope.series.splice(index, 1);
